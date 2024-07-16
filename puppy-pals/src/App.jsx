6a29703e4dd3539a3669fbbdf58c7e39f0ba42cf
@paulywrath <127055825+paulywrath@users.noreply.github.com>
2024-07-16 16:24:55 -0400

    Render puppy names

--- puppy-pals/src/App.jsx
+++ puppy-pals/src/App.jsx
@@ -1,12 +1,22 @@
+import { useState } from 'react'
 import {puppyList} from './data.js'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [puppies, setPuppies] = useState(puppyList);
+
+  console.log("puppyList: ", puppyList);
 
   return (
-    <div></div>
-  )
+    <div className="App">
+      {
+        puppies.map((puppy) => {
+             return <p key={puppy.id}>{puppy.name}</p>;
+           })
+       }
+    </div>
+  );
 }
 
 export default App
